dcd8f263566c97962fa540d0f024f58d79cc838c
CC <colin@szecol.in>
2020-07-12 03:40:44 +0000

    cleanup script; add ansible to base image

--- start.sh
+++ start.sh
@@ -3,9 +3,8 @@
 alias myloc='curl -s ifconfig.co/country'
 alias myip='curl -s ifconfig.co'
 
-_vpnfiles=/ovpn-files
 _outfile=/tmp/vpnenv
-./openvpn/random-vpn.sh --ovpn=$_vpnfiles --out_envfile=$_outfile
+./openvpn/random-vpn.sh --ovpn=/ovpn-files --out_envfile=$_outfile
 source $_outfile
 echo "Set" $(env |grep VPN_)
 ./qbtorrent/iptables.sh
