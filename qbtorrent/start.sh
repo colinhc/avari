0ddae6b9d2a232a30629ff48fd5d3f1236ce2ed2
CC <colin@szecol.in>
2023-07-24 04:11:08 +0000

    Switch openvpn over to wg

--- qbtorrent/start.sh
+++ qbtorrent/start.sh
@@ -5,45 +5,6 @@ if [[ ! -e $AVARI_QBT_HOME/qbtorrent.conf ]]; then
 	exit 1
 fi
 
-# set umask
-export UMASK=$(echo "${UMASK}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
-
-if [[ ! -z "${UMASK}" ]]; then
-  echo "[info] UMASK defined as '${UMASK}'" | ts '%Y-%m-%d %H:%M:%.S'
-else
-  echo "[warn] UMASK not defined (via -e UMASK), defaulting to '002'" | ts '%Y-%m-%d %H:%M:%.S'
-  export UMASK="002"
-fi
-
-# Set qBittorrent WebUI and Incoming ports
-if [ ! -z "${WEBUI_PORT}" ]; then
-	webui_port_exist=$(cat $AVARI_QBT_HOME/qbtorrent.conf | grep -m 1 'WebUI\\Port='${WEBUI_PORT})
-	if [[ -z "${webui_port_exist}" ]]; then
-		webui_exist=$(cat $AVARI_QBT_HOME/qbtorrent.conf | grep -m 1 'WebUI\\Port')
-		if [[ ! -z "${webui_exist}" ]]; then
-			# Get line number of WebUI Port
-			LINE_NUM=$(grep -Fn -m 1 'WebUI\Port' $AVARI_QBT_HOME/qbtorrent.conf | cut -d: -f 1)
-			sed -i "${LINE_NUM}s@.*@WebUI\\Port=${WEBUI_PORT}@" $AVARI_QBT_HOME/qbtorrent.conf
-		else
-			echo "WebUI\Port=${WEBUI_PORT}" >> $AVARI_QBT_HOME/qbtorrent.conf
-		fi
-	fi
-fi
-
-if [ ! -z "${INCOMING_PORT}" ]; then
-	incoming_port_exist=$(cat $AVARI_QBT_HOME/qbtorrent.conf | grep -m 1 'Connection\\PortRangeMin='${INCOMING_PORT})
-	if [[ -z "${incoming_port_exist}" ]]; then
-		incoming_exist=$(cat $AVARI_QBT_HOME/qbtorrent.conf | grep -m 1 'Connection\\PortRangeMin')
-		if [[ ! -z "${incoming_exist}" ]]; then
-			# Get line number of Incoming
-			LINE_NUM=$(grep -Fn -m 1 'Connection\PortRangeMin' $AVARI_QBT_HOME/qbtorrent.conf | cut -d: -f 1)
-			sed -i "${LINE_NUM}s@.*@Connection\\PortRangeMin=${INCOMING_PORT}@" $AVARI_QBT_HOME/qbtorrent.conf
-		else
-			echo "Connection\PortRangeMin=${INCOMING_PORT}" >> $AVARI_QBT_HOME/qbtorrent.conf
-		fi
-	fi
-fi
-
 echo "[info] Starting qBittorrent daemon..." | ts '%Y-%m-%d %H:%M:%.S'
 /bin/bash $AVARI_QBT_HOME/qbittorrent.init start &
 
---
+++ setup_network.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Pre-defined env variables:
+#   $HOST_NETWORK
+#   $WG_GATWAY
+
+source logging
+
+DEFAULT_GATEWAY=$(ip route show | grep default | awk '{print $3}')
+UNEXPECTED_IP=$(curl -s ifconfig.co)
+
+ip route del default
+ip route add default via "$WG_GATEWAY"
+ip route add "$HOST_NETWORK" via "$DEFAULT_GATEWAY"
+
+ufw default deny incoming
+ufw default allow outgoing
+ufw deny out to "$HOST_NETWORK"
+ufw allow in from "$HOST_NETWORK"
+ufw enable
+
+while : ; do
+	actual_ip=$(curl -s ifconfig.co)
+	actual_city=$(echo "$(curl -s ifconfig.co/country): $(curl -s ifconfig.co/city)")
+	if [[ $actual_ip != $UNEXPECTED_IP ]]; then
+		loginfo "Assigned ip: $actual_ip $actual_city"
+		break
+	fi
+	loginfo "Unexpected: $UNEXPECTED_IP; Actual: $actual_ip"
+	sleep 3m
+done
